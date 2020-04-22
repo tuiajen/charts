4d155abd646bd4ddbe9d3e9b1a3b7e387aa19585
tuiajen <tuiajen@yahoo.com>
2020-04-22 13:27:46 -0700

    Update waffle.R

--- waffle.R
+++ waffle.R
@@ -10,7 +10,7 @@ df <- read_excel("waffletemplate.xlsx") %>% clean_names() #fix headers on arriva
 
 my_percent <-  .46  #Shaded area and under so 80% is what I'm highlighting
 my_plot <- ggplot(data = df, 
-                       aes(x = column, y = row, label = percentage))
+                       aes(x = column, y = row, label = percentage)) #not using label at this time
 
 #label = rownames(mtcars)
 # Draw the plot
@@ -30,22 +30,5 @@ my_plot +
     axis.ticks.y=element_blank())
 dev.copy(png,'myplot.png')
 dev.off()
- # scale_y_reverse() 
- # geom_text(aes(percentage))
+
  
-    
-
-#ggplot(np_graph) + geom_point(aes(x = C1, y = C2, colour = C1 >0)) +
-#  scale_colour_manual(name = 'PC1 > 0', values = setNames(c('red','green'),c(T, F))) +
-#  xlab('PC1') + ylab('PC2')
-
-
-#panel.spacing=5
-   # panel.grid.major = element_line(colour = "grey50"),
-   # panel.ontop = TRUE
-  
-  #theme_dark()+
-  #element_rect(fill = NULL, colour = NULL, size = NULL, linetype = NULL, color = NULL)
-p <- ggplot(mtcars, aes(wt, mpg, label = rownames(mtcars)))
-
-p + geom_point() + geom_text()
